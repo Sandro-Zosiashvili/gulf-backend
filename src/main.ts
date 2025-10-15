f1b510465105ae6121f4232f596be46e7c54b081
Sandro-Zosiashvili <zosiashvilisavle@gmail.com>
2025-10-15 19:53:39 +0400

    Feat: usePipe and cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  });
+  app.useGlobalPipes();
+
   await app.listen(process.env.PORT ?? 3001);
+
 }
 bootstrap();
