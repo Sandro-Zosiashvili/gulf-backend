12d7b132fc0862797faa76b1ca770f107c6d0b26
Sandro-Zosiashvili <zosiashvilisavle@gmail.com>
2025-10-18 17:37:22 +0400

    Refactor: prices

--- src/price/price.service.ts
+++ src/price/price.service.ts
@@ -15,28 +15,28 @@ export class PriceService {
             }, {
                 id: 2,
                 title: "G-Force პრემიუმი",
-                price: "3.74₾"
+                price: "3.39₾"
             }, {
                 id: 3,
                 title: "G-Force რეგულარი",
-                price: "3.74₾"
+                price: "3.24₾"
             }, {
                 id: 4,
                 title: "ევრო რეგულარი",
-                price: "3.74₾"
+                price: "3.09₾"
             }, {
                 id: 5,
                 title: "G-Force ევრო დიზელი",
-                price: "3.74₾"
+                price: "4.09₾"
             },
             {
                 id: 6,
                 title: "ევრო დიზელი",
-                price: "3.74₾"
+                price: "3.99₾"
             }, {
                 id: 7,
                 title: "გაზი",
-                price: "3.74₾"
+                price: "1.69₾"
             },
         ]
         return data;
